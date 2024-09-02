507f76f562f7b4d685b139f0695fabeb360512c0
codeMaverick7770 <Mtauqeer7770@gmail.com>
2024-09-02 14:24:36 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+HYPEN_E_DOT='-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    function to return list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+
+
+setup(
+name='chatBOT',
+version='0.0.1',
+author='codeMaverick7770',
+author_email='Mtauqeer7770@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
